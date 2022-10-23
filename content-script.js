f7d608174ffc9f5c9d0b02783ac275522d8b11ad
Алексей <au_au_201313@bk.ru>
2022-10-23 15:03:34 +0300

    Промужуточное

--- content-script.js
+++ content-script.js
@@ -25,3 +25,33 @@ Not_search_mastergroup.setAttribute("id", "no-pay-mastergroups")
 Not_search_mastergroup.innerText = chrome.runtime.getURL("/images/Not-search-mastergroup.png");
 Not_search_mastergroup.style.display = "none";
 (document.head).appendChild(Not_search_mastergroup)
+
+function add_to_storage() {
+    chrome.storage.local.set({"mastergroup": true, "course": false}), function(){
+        console.log("Успешное добавление в локальное хранилище");
+    }
+}
+
+chrome.storage.local.get([ "mastergroup" , "course" ], function(result) {
+    if (result.mastergroup == undefined || result.course == undefined ) {
+        add_to_storage()
+    } else if ( result.mastergroup == true && result.course == true ) {
+        add_to_storage()
+    } else if ( result.mastergroup == false && result.course == false ) {
+        add_to_storage()
+    }
+});
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        console.log("Content script received message: " + event.data.text);
+        chrome.storage.local.get([ "mastergroup" , "course" ], function(result) {
+            console.log("in ...", {"mastergroup": result.mastergroup, "course": result.course});
+            return {"mastergroup": result.mastergroup, "course": result.course}
+        })
+    }
+});
---
+++ popup/popup.js
@@ -0,0 +1,35 @@
+function add_to_storage() {
+    chrome.storage.local.set({"mastergroup": true, "course": false}), function(){
+        console.log("Успешное добавление в локальное хранилище");
+    }
+    document.getElementById('popup_mastergroup').checked = true;
+    document.getElementById('popup_course').checked = false;
+
+}
+
+chrome.storage.local.get([ "mastergroup" , "course" ], function(result) {
+    if (result.mastergroup == undefined || result.course == undefined ) {
+        add_to_storage()
+    } else if ( result.mastergroup == true && result.course == true ) {
+        add_to_storage()
+    } else if ( result.mastergroup == false && result.course == false ) {
+        add_to_storage()
+    } else {
+        document.getElementById('popup_mastergroup').checked = result.mastergroup;
+        document.getElementById('popup_course').checked = result.course;
+    }
+
+});
+
+document.getElementById('popup_mastergroup').onclick = function() {
+    chrome.storage.local.set({"mastergroup": true, "course": false}), function(){
+        console.log("Успешное добавление в локальное хранилище");
+    }
+}
+
+document.getElementById('popup_course').onclick = function() {
+    chrome.storage.local.set({"mastergroup": false, "course": true}), function(){
+        console.log("Успешное добавление в локальное хранилище");
+    }
+}
+
