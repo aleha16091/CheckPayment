371a7211b17fdf54e9fcb6119ada9b8edd0ae078
Алексей <au_au_201313@bk.ru>
2022-10-24 18:21:44 +0300

    2

--- js/content-script.js
+++ js/content-script.js
@@ -1,13 +1,13 @@
 function add_to_dom() {
     var script = document.createElement('script');
-    script.src = chrome.runtime.getURL('js/script.js');
+    script.src = chrome.runtime.getURL('insert.js');
     script.onload = function() {
         this.remove();
     };
     (document.head || document.documentElement).appendChild(script);
 
     var css = document.createElement('link');
-    css.src = chrome.runtime.getURL('insert.css');
+    css.src = chrome.runtime.getURL('../css/insert.css');
     css.rel = 'stylesheet'
     css.type = 'text/css'
     css.onload = function() {
@@ -17,13 +17,13 @@ function add_to_dom() {
 
     var Not_search_course = document.createElement('div')
     Not_search_course.setAttribute("id", "no-pay-courses")
-    Not_search_course.innerText = chrome.runtime.getURL("/images/Not-search-course.png");
+    Not_search_course.innerText = chrome.runtime.getURL("../images/Not-search-course.png");
     Not_search_course.style.display = "none";
     (document.head).appendChild(Not_search_course)
 
     var Not_search_mastergroup = document.createElement('div')
     Not_search_mastergroup.setAttribute("id", "no-pay-mastergroups")
-    Not_search_mastergroup.innerText = chrome.runtime.getURL("/images/Not-search-mastergroup.png");
+    Not_search_mastergroup.innerText = chrome.runtime.getURL("../images/Not-search-mastergroup.png");
     Not_search_mastergroup.style.display = "none";
     (document.head).appendChild(Not_search_mastergroup)
 }
